a7f0901aa97722ecfaac38080f256c6f1e48a5e1
unknown <17621158197@crs.prod>
2020-04-16 16:47:25 +0800

    优化脚本

--- bukesi.py
+++ bukesi.py
@@ -1,36 +1,36 @@
 # -*- coding: utf-8 -*-
 2
-3  #------------------------------------
+3  # ------------------------------------
 4  # Name:         bukesi
 5  # Description:  
 6  # Author:       kingming
 7  # Date:         2020/4/10
-8  #------------------------------------
+8  # ------------------------------------
 
 import time
 import json
 import struct
 from socket import *
 from threading import Thread
+from configRead import Read
+# 读取配置文件
+R = Read('config.ini')
+server_ip = R.getInfo('info', 'server_ip')
 
-server_ip = '192.168.50.138' # 修改为服务器 ip
-# Mac
-host_mac = '00:8B:92:3E:56:33' # 修改为本机 mac
-robot_mac = '02:38:4B:4A:55:0C' # 修改为实际机器人的 mac
 
-# 命令
-reg_client_message = {
- "message_type": "register_client",
- "client_type": 3,
- "mac_address": host_mac}
-get_all_robot_info_message = {
- "message_type": "get_all_robot_info",}
-move_message = {
- "message_type": "move",
- "robot_mac_address": robot_mac,
- "vx": 0,
- "vy": 0,
- 'vtheta': 0.2}
+# 过滤消息
+set_filter = {
+    "message_type": "set_filter",
+    "filter": ["laser", "all_file_info", "report_sensor_data_info", "set_sensor_data_info", "report_basic_status"
+        , "report_obd_status", 'sensor_power_status', 'report_stat', 'report_pos_vel_status', 'auto_guided_task_status'
+        , 'device_status', 'local_path', 'register_status', 'all_robot_info', 'sonar']
+}
+def getDate():
+    return time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())
+
+def writeLog(content):
+    f1 = open('logs/boocax_'+time.strftime('%Y-%m-%d', time.localtime())+'.log', 'a+')
+    f1.write(getDate()+':'+str(content)+'\n')
 
 def receive2dic(tcp_sock):
     """ 以字节方式（bytes）接收数据，
@@ -69,30 +69,107 @@ class RecvTread(Thread):
     def run(self):
         while True:
             msg = receive2dic(self.s)
-            time.sleep(1)
+            # time.sleep(1)
             # print(msg['message_type'])
             print msg
+
 
 if __name__ == '__main__':
     # Socket TCP
     s = socket(AF_INET, SOCK_STREAM)
     s.connect((server_ip, 6789))
 
-    # Send
-    json_packed = pack2bytes(reg_client_message)  # 注册客户端
+    # status
+    json_packed = pack2bytes(R.getInfo('status', 'reg_client_message'))  # 注册客户端
     s.send(json_packed)
-    # json_packed = pack2bytes(get_all_robot_info_message)  # 获取全部机器人信息
+    # json_packed = pack2bytes(set_filter)  # 过滤消息
+    # s.send(json_packed)
+    # json_packed = pack2bytes(R.getInfo('status', 'get_all_robot_info_message'))  # 获取全部机器人信息
+    # s.send(json_packed)
+    # json_packed = pack2bytes(R.getInfo('status', 'report_loc_status'))  # 定位状态
+    # s.send(json_packed)
+    json_packed = pack2bytes(R.getInfo('status', 'move_status_v2'))  # 移动状态
+    s.send(json_packed)
+    json_packed = pack2bytes(R.getInfo('status', 'charge_status'))  # 充电状态
+    s.send(json_packed)
+
+    # control
+    # json_packed = pack2bytes(R.getInfo('status', 'move_message'))  # 移动控制一段距离
+    # s.send(json_packed)
+    # json_packed = pack2bytes(R.getInfo('status', 'poi_action'))  # 导航点指定点
+    # s.send(json_packed)
+    # json_packed = pack2bytes(R.getInfo('control', 'goCharge'))  # 导航到对桩点并充电
+    # s.send(json_packed)
+    # json_packed = pack2bytes(R.getInfo('control', 'cancel_charge')) # 取消充电
     # s.send(json_packed)
 
+
     # 启动接收线程
-    RecvTread(s).start()
+    # RecvTread(s).start()
 
-    # 移动控制
-    # json_packed = pack2bytes(move_message)
-    # while True:
-    #     s.send(json_packed)
-    #     time.sleep(0.2)
-        # time.sleep(1000)
+    i = 1
+    fail = 0
+    while i <= 10:
+        print '*****************开始第%d轮导航****************************'%i
+        writeLog('*****************开始第%d轮导航****************************'%i)
+        # 导航到p4
+        print '导航到p1...'
+        writeLog('导航到p1...')
+        json_packed = pack2bytes(R.getInfo('control', 'poi_action'))  # 导航点指定点
+        s.send(json_packed)
+        while True:
+            msg = receive2dic(s)
+            writeLog(msg)
+            if msg.get('move_status') == 300:
+                print '到达目的地'
+                writeLog('到达目的地')
+                break
+            else:
+                print '导航中...'
+                continue
 
+        # 导航到p5
+        print '导航到p2...'
+        writeLog('导航到p2...')
+        json_packed = pack2bytes(R.getInfo('control', 'poi_action1'))  # 导航点指定点
+        s.send(json_packed)
+        while True:
+            msg = receive2dic(s)
+            writeLog(msg)
+            if msg.get('move_status') == 300:
+                print '到达目的地'
+                writeLog('到达目的地')
+                break
+            else:
+                print '导航中...'
+                continue
+
+        # 充电
+        print '导航到充电点并充电'
+        writeLog('导航到充电点并充电')
+        json_packed = pack2bytes(R.getInfo('control', 'goCharge'))  # 导航到对桩点并充电
+        s.send(json_packed)
+        while True:
+            msg = receive2dic(s)
+            if msg.get('charge_status') == 1:
+                print '正在使用充电桩充电(自动对接)'
+                writeLog('正在使用充电桩充电(自动对接)')
+                break
+            elif msg.get('charge_status') == 0:
+                print '充电失败'
+                writeLog('充电失败')
+                fail += 1
+                break
+            else:
+                print '导航到充电桩...'
+                continue
+        time.sleep(3)
+        json_packed = pack2bytes(R.getInfo('control', 'cancel_charge'))
+        s.send(json_packed)
+        json_packed = pack2bytes(R.getInfo('control', 'poi_action'))
+        i += 1
+    print '*********************已完成{}轮导航,充电失败{}次*****************************'.format(i, fail)
+    writeLog('*********************已完成{}轮导航,充电失败{}次*****************************'.format(i, fail))
     # Close
-    # s.close()
+    s.close()
+
---
+++ configRead.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-#
+2
+3  #------------------------------------
+4  # Name:         configRead
+5  # Description:  
+6  # Author:       kingming
+7  # Date:         2020/4/14
+8  #------------------------------------
+
+import configparser
+import re
+
+
+class Read():
+    def __init__(self, file):
+        self.cfg = configparser.ConfigParser()
+        self.cfg.read('config.ini', encoding="utf-8")
+
+    def getInfo(self, c1, c2):
+        if c1 == 'info':
+            return self.cfg.get(c1, c2).encode('utf-8')
+        elif c1 == 'status':
+            return eval(self.cfg.get(c1, c2).encode('utf-8').replace('host_mac', self.getInfo('info', 'host_mac')))
+        elif c1 == 'control':  # 正则匹配
+            rec = re.search('"robot_mac_address":(.*),', self.cfg.get(c1, c2).encode('utf-8')).group(1)
+            return eval(re.sub(rec, self.getInfo('info', 'robot_mac1'), self.cfg.get(c1, c2).encode('utf-8')))
+        else:
+            print '没有匹配到结果...'
+
+
+# if __name__ == '__main__':
+#     R = Read('config.ini')
+#     print R.getInfo('control', 'cancel_charge')
+
